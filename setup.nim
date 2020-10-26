ad8900a2d7fb69729d85a7cab9ee2520da0bf3ed
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2020-10-26 06:26:02 -0700

    Added the assumption the containers are installed

--- setup.nim
+++ setup.nim
@@ -8,12 +8,10 @@ writeFile("hashLib.lua", content)
 
 discard execCmd "cabal install megaparsec"
 discard execCmd "cabal install text"
-discard execCmd "cabal install containers"
 discard execCmd "cabal install hashable"
 
 discard execCmd "cabal install megaparsec --lib"
 discard execCmd "cabal install text --lib"
-discard execCmd "cabal install containers --lib"
 discard execCmd "cabal install hashable --lib"
 
 discard execCmd "cabal repl"
