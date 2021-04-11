b4cf0b1cf90d8657cb9688421a6504fbf955932d
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2021-04-10 18:13:17 -0700

    Added isNum and fixed empty function

--- libs/std/intify.js
+++ libs/std/intify.js
@@ -1,16 +1,20 @@
-import {SltThunk, SltFunc, SltNum, SltValue, error} from "../../SltRuntime.ts"
+import {SltThunk, SltFunc, SltNum, SltBool, error} from "../../SltRuntime.ts"
 
-const isNum = n => !isNaN(parseFloat(n().value))
+const isNum = n => !isNaN(n().toString()) && !isNaN(parseFloat(n().toString()))
+
+const isNumBase = new SltThunk(
+    () => new SltFunc(n => new SltBool(isNum(n), n().loc), [7, 2, "intify.js"])
+)
 
 const stringToInt = new SltThunk(
     () =>
         new SltFunc(
             i => {
                     if (isNum(i)) error("TypeError: ", "First argument is not a string", i())
-                    else return new SltNum(i().value.parseFloat(), [6, 1, "intify.lua"])
+                    else return new SltNum(i().value.parseFloat(), [6, 1, "intify.js"])
                 }, 
-                [2, 5, "intify.lua"]
+                [2, 5, "intify.js"]
             )
 )
 
-export {stringToInt, isNum}
+export {stringToInt, isNumBase}
--- libs/std/intify.lua
+++ libs/std/intify.lua
@@ -1,12 +1,20 @@
 require "SltRuntime"
 
-function isNum(n) return tonumber(n().value) == nil end
+function isNumLua(n) return tonumber(n().value) == nil end
+
+isNumBase = SltThunk.create(
+    function() return
+        SltFunc.create(
+            function(n) return SltBool.create(tonumber(tostring(n())) ~= nil, n().loc) end
+        )
+    end
+)
 
 stringToInt = SltThunk.create(
     function() return
         SltFunc.create(
             function(i)
-                if isNum(i) then 
+                if isNumLua(i) then 
                     return SltError.create("TypeError: ", "First argument is not a string", i())
                 else
                     return SltNum.create(tonumber(i().value), {6, 1, "intify.lua"})
