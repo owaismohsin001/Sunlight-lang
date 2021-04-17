2b69bc48878d93b99b0a04e0beddf7816b800298
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2021-04-17 05:48:41 -0700

    round function and integer division

---
+++ libs/std/basicMath.js
@@ -0,0 +1,23 @@
+import {SltFunc, SltThunk, SltNum} from '../../SltRuntime.ts'
+
+const unsafeExponent = new SltThunk(
+    () =>
+        new SltFunc(
+            a =>
+                new SltFunc(
+                    b => new SltNum(a().value ** b().value, a().pos),
+                    [7, 1, "intify.lua"]
+                ),
+                [7, 1, "intify.lua"]
+            ),
+)
+
+const unsafeRound = new SltThunk(
+    () => 
+        new SltFunc(
+            a => new SltNum(Math.round(a().value), a().pos),
+            [7, 1, "intify.lua"]
+        )
+)
+
+export {unsafeExponent, unsafeRound}
---
+++ libs/std/basicMath.lua
@@ -0,0 +1,31 @@
+require 'SltRuntime'
+
+function round(x)
+    return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+end
+
+unsafeRound = SltThunk.create(
+    function() return
+        SltFunc.create(
+            function(a) return
+                    SltNum.create(round(a().value), a().pos)
+            end,
+            {7, 1, "intify.lua"}
+        )
+    end
+)
+
+unsafeExponent = SltThunk.create(
+    function() return
+        SltFunc.create(
+            function(a) return
+                SltFunc.create(
+                    function(b) return
+                        SltNum.create(math.pow(a().value, b().value), a().pos)
+                    end
+                )
+            end
+            ),
+            {7, 1, "intify.lua"}
+    end
+)
